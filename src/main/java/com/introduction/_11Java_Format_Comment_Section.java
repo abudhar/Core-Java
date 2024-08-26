33370b1eead170afc7f2b95adf1a25ffdcbd0c7f
QICHOAPPL\mohammed.abudhar <mohammed.abudhar@anoudtechnologies.com>
2024-08-26 09:11:02 +0530

    Comments

---
+++ src/main/java/com/introduction/_11Java_Format_Comment_Section.java
@@ -0,0 +1,57 @@
+package com.introduction;
+
+
+public class _11Java_Format_Comment_Section {
+	/*
+	 * Five Section: 
+	 *  	- Comment Section
+	 *  		-> used for Information Purpose
+	 *  		-> Types : 
+	 *  				1. Single Line Comment -> //
+	 *  				2. Multi Line Comment  -> /* */
+	/*  				3. Documentation Comment ->  /** * * */
+	/*  	- Package Section
+	 *  	- Import Section
+	 *  	- Class/Interface Section
+	 *  	- Main Class Section
+	 *  
+	 *  	Documentation for API Documentation is Not Recommended, Because Java Provoided "javadoc".
+	 *  
+	 *  	In Eclipse, File -> Export -> JavaDoc
+	 *  
+	 *  	Eg: /doc Folder for Employee Class
+	 *  
+	 *  	To Provide to Desc/metadata JDK 1.5 version given @ annotation
+	 *  
+	 *  	Why Annotations instead of Comments?
+	 *  		-> Comments will Available only till .java class
+	 *  		-> AFter Compiling .class file it will be removed so we will use annotation
+	 *  		->@annotation will provide metadata while runtime eg, Hibernate
+	 *  	
+	 *  	To provide metadata in runtime of application, ALready XML in Available why annotation?
+	 *  		-> XML in not java ,To omit XML java introduced annotations
+	 *  		-> we need to ensure whether XML is there not
+	 *  		-> we need to ensure valid or not
+	 *  		-> we need to ensure Right Parsing
+	 *  
+	 *  	XML BASED TECH STACK:
+	 *  		-> upto JDK 1.4
+	 *  		-> JDBC 3.0
+	 *  		-> Servlet 2.x (web.xml)
+	 *  		-> Struts 1.x (struts.xml)
+	 *  		-> JSF 1.x
+	 *  		-> EJBs 2.x
+	 *  		-> Spring 2.4
+	 *  	@ BASED TECH STACK:
+	 *  		-> upto JDK 1.5 and Above
+	 *  		-> JDBC 4.0 and Above
+	 *  		-> Servlet 3.x 
+	 *  		-> Struts 2.x 
+	 *  		-> JSF 2.x
+	 *  		-> EJBs 3.x
+	 *  		-> Spring 2.5 (stabled annotaion at 3.5 version)
+	 *  	
+	 */
+	
+	
+}
---
+++ src/main/java/com/introduction/_12Java_Format_Package.java
@@ -0,0 +1,26 @@
+package com.introduction;
+
+public class _12Java_Format_Package {
+	/*
+	 * Package are Colletions of classed/Interface as single unit (Folder)
+	 * 
+	 * 	Eg:
+	 * 		-> Java.io
+	 * Two Types :-
+	 * 		1. Predefined (already Java created)
+	 * 		2. User Defined (devoloped by devolopers)
+	 * 
+	 * Q) Possible to declare more than one package?
+	 * 
+	 * 		no, it need to be on first line
+	 * 
+	 * Naming Convention : web site name/domain as reverse
+	 * 
+	 * 	www.newcollege.com
+	 *  package name -> com.newcollege
+	 *  project name -> com.newcollege.exam
+	 *  module name  -> com.newcollege.exam.question
+	 *  sub module name -> com.newcollege.exam.question.aptitude
+	 *  
+	 */
+}
